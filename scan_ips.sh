a5afae30ca201c028f0cdefc9f700401b6530a19
Daniel Sánchez <146337705+danisanjp@users.noreply.github.com>
2024-02-15 16:50:25 +0100

    Update scan_ips.sh

--- scan_ips.sh
+++ scan_ips.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+echo "Introduce la primera IP"
+read FirstIP
 
+echo "Introduce la ultima IP"
+read LastIP
+
+echo "Introduce el puerto que quieres comprobar"
+read port
+
+nmap -sT $FirstIP-$LastIP -p $port -oG web
+cat web | grep open >web1
+cat web1 |cut -f2 -d":" | cut -f1 -d"(" > web2
+cat web2
